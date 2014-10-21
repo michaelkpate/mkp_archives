520cb7893499f9b6eb1fa3384fb9baeb618ae68e
Michael K Pate <michaelkpate@gmail.com>
2014-10-21 12:39:47 -0400

    Create mkp_archives.php

---
+++ mkp_archives.php
@@ -0,0 +1,21 @@
+	function mkp_archive()
+	{
+
+		global $variable;
+	
+		$url = preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI'));
+
+		$parts = explode('/', $url, 5);
+
+		$variable['y'] = $parts[1];
+
+		$variable['m'] = $parts[2];
+
+		$variable['d'] = $parts[3];
+
+		$month_num = variable(array("name" => "m"));
+
+		$variable['monthtitle'] = (!empty($month_num)) ? date('F', mktime(0, 0, 0, $month_num)) : null;
+		 
+		return null;
+	}
