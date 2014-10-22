ac942d62f8423927378cb260b5b0c1a3363f9abf
Michael K Pate <michaelkpate@gmail.com>
2014-10-22 17:35:48 -0400

    Update mkp_url_parameters.php

--- mkp_url_parameters.php
+++ mkp_url_parameters.php
@@ -1,19 +1,19 @@
-	function mkp_url_parameters()
-	{
+function mkp_url_parameters()
+{
 
-		global $variable;
+	global $variable;
 
-		// preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')) was copied from the function preText in publish.php
+	// preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')) was copied from the function preText in publish.php
 
-		$parts = explode('/', preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')), 5);
+	$parts = explode('/', preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')), 5);
 
-		$variable['y'] = (is_numeric($parts[1]));
+	$variable['y'] = (is_numeric($parts[1]));
 
-		$variable['m'] = (is_numeric($parts[2]));
+	$variable['m'] = (is_numeric($parts[2]));
 
-		$variable['d'] = (is_numeric($parts[3]));
+	$variable['d'] = (is_numeric($parts[3]));
 
-		$variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null;
-		 
-		return null;
-	}
+	$variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null;
+	 
+	return null;
+}
