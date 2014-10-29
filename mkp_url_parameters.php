1419f3f4c68648edac7ca22f2574db618e35ed47
Michael K Pate <michaelkpate@gmail.com>
2014-10-29 17:46:02 -0400

    Update mkp_url_parameters.php

--- mkp_url_parameters.php
+++ mkp_url_parameters.php
@@ -7,13 +7,32 @@ function mkp_url_parameters()
 
 	$parts = explode('/', preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')), 5);
 
-	$variable['y'] = (is_numeric($parts[1])) ? $parts[1] : null;
+	if (is_numeric($parts[1])) {
 
-	$variable['m'] = (is_numeric($parts[2]))? $parts[2] : null;
+	// y is numeric year, m is numeric month, monthtitle is name of month, d is numeric day 
 
-	$variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null;
+		$variable['y'] = (is_numeric($parts[1])) ? $parts[1] : null;
 
-	$variable['d'] = (is_numeric($parts[3]))? $parts[3] : null;
+		$variable['m'] = (is_numeric($parts[2]))? $parts[2] : null;
+
+		$variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null;
+
+		$variable['d'] = (is_numeric($parts[3]))? $parts[3] : null;
+
+	} else {
+
+	// at this point we are going to assume that Textpattern is running in a subdirectory and shift to the right in our array
+
+ 	 	$variable['y'] = (is_numeric($parts[2])) ? $parts[2] : null;
+
+		$variable['m'] = (is_numeric($parts[3]))? $parts[3] : null;
+
+		$variable['monthtitle'] = (is_numeric($parts[3])) ? date('F', mktime(0, 0, 0, $parts[3])) : null;
+
+		$variable['d'] = (is_numeric($parts[4]))? $parts[4] : null;
+
+	} 
 	 
 	return null;
 }
+
