e9e16d8f2105f5b3b02c83c026849050b1594863
Michael K Pate <michaelkpate@gmail.com>
2016-06-03 11:24:11 -0400

    Update mkp_url_parameters.php
    added Textpattern 4.6 Tag Registration and uses isset to prevent PHP errors from being generated

--- mkp_url_parameters.php
+++ mkp_url_parameters.php
@@ -1,3 +1,10 @@
+// TXP 4.6 tag registration
+if (class_exists('\Textpattern\Tag\Registry')) {
+Txp::get('\Textpattern\Tag\Registry')
+->register('mkp_url_parameters')
+;
+}
+
 function mkp_url_parameters()
 {
 
@@ -11,28 +18,27 @@ function mkp_url_parameters()
 
 	// y is numeric year, m is numeric month, monthtitle is name of month, d is numeric day 
 
-		$variable['y'] = (is_numeric($parts[1])) ? $parts[1] : null;
+		if (isset($parts[1])) { $variable['y'] = (is_numeric($parts[1])) ? $parts[1] : null; }
 
-		$variable['m'] = (is_numeric($parts[2]))? $parts[2] : null;
+		if (isset($parts[2])) { $variable['m'] = (is_numeric($parts[2]))? $parts[2] : null; }
 
-		$variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null;
+		if (isset($parts[2])) { $variable['monthtitle'] = (is_numeric($parts[2])) ? date('F', mktime(0, 0, 0, $parts[2])) : null; }
 
-		$variable['d'] = (is_numeric($parts[3]))? $parts[3] : null;
+		if (isset($parts[3])) { $variable['d'] = (is_numeric($parts[3]))? $parts[3] : null; }
 
 	} else {
 
 	// at this point we are going to assume that Textpattern is running in a subdirectory and shift to the right in our array
 
- 	 	$variable['y'] = (is_numeric($parts[2])) ? $parts[2] : null;
+		if (isset($parts[2])) { $variable['y'] = (is_numeric($parts[2])) ? $parts[2] : null; }
 
-		$variable['m'] = (is_numeric($parts[3]))? $parts[3] : null;
+		if (isset($parts[3])) { $variable['m'] = (is_numeric($parts[3]))? $parts[3] : null; }
 
-		$variable['monthtitle'] = (is_numeric($parts[3])) ? date('F', mktime(0, 0, 0, $parts[3])) : null;
+		if (isset($parts[3])) { $variable['monthtitle'] = (is_numeric($parts[3])) ? date('F', mktime(0, 0, 0, $parts[3])) : null; }
 
-		$variable['d'] = (is_numeric($parts[4]))? $parts[4] : null;
+		if (isset($parts[4])) { $variable['d'] = (is_numeric($parts[4]))? $parts[4] : null; }
 
 	} 
 	 
 	return null;
 }
-
